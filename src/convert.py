1366dbf0d68235fa32f0c562ec81795bfa9455b2
daikichidaze <dkch.yoshioka.t@gmail.com>
2021-02-26 21:32:32 +0700

    change user master

--- src/convert.py
+++ src/convert.py
@@ -13,6 +13,7 @@ from bs4 import BeautifulSoup
 import xmltodict
 
 num_of_topics = 5
+num_of_pages_in_xml = 10  # 1xmlファイルあたりのページ数
 
 
 class PageDataContainer:
@@ -31,51 +32,68 @@ class PageDataContainer:
 
         # 質問したメンバーの表示名
         question_talk = df_thread.iloc[0]
+        self.question_user_real_name = user_master[question_talk['target_date']].get_real_name(
+            question_talk['user_id'])
         self.question_member = question_talk['user_name']
         # 回答したメンバーの表示名のlist
         if len(df_thread) < 2:
+            self.answer_user_real_names = []
             self.answer_members = []
         elif len(df_thread) == 2:
-            self.answer_members = [df_thread.loc[1, 'user_name']]
+            answer_talk = df_thread.loc[1]
+            self.answer_user_real_names = [
+                user_master[answer_talk['target_date']
+                            ].get_real_name(answer_talk['user_id'])
+            ]
+            self.answer_members = [answer_talk['user_name']]
         else:
-            self.answer_members = df_thread.loc[1:]['user_name'].tolist()
+            answer_talks = df_thread.loc[1:]
+            self.answer_user_real_names = answer_talks.apply(
+                lambda r: user_master[r.target_date].get_real_name(r.user_id), axis=1).tolist()
+            self.answer_members = answer_talks['user_name'].tolist()
 
-        self.tech_topics = self.annotate_topics(
+        topic_terms = self.find_topic_terms(
             df_thread['talk_text'].tolist(), topic_detector)
+        self.tech_topics = topic_terms[:num_of_topics]
 
         # title_idxから"Q&A-xxxx"を設定
-        self.title = f'Q&A-{i:06d}:' + '-'.join(self.tech_topics)
+        self.title = f'Q&A-{i:06d}:' + '-'.join(sorted(self.tech_topics))
 
         df_thread['talk_text_rpls'] = df_thread.apply(
             lambda r: replace_username(r.talk_text, user_master.get(r.target_date)), axis=1)
 
-        # 質問本文
-        self.question_contents = df_thread.loc[0, 'talk_text_rpls']
-        # 回答本文
-        self.answer_contents = df_thread.loc[1:, 'talk_text_rpls'].tolist()
+        # annotate
+        for term in topic_terms:
+            df_thread['talk_text_rpls'] = \
+                df_thread['talk_text_rpls'].str.replace(
+                    term,  f'[[質問トピック::{term}]]', regex=False)
 
-    def annotate_topics(self, text_list, topic_detector):
+        self.question_contents = df_thread.loc[0, 'talk_text_rpls']  # 質問本文
+        self.answer_contents = \
+            df_thread.loc[1:, 'talk_text_rpls'].tolist()  # 回答本文
+
+    def find_topic_terms(self, text_list, topic_detector):
         all_text = '\n'.join(text_list)
         retxt = topic_detector.replace_inconsistencies(
             all_text, topic_detector.str_replace_dict)
         retxt = topic_detector.delete_symbols(all_text)
 
         doc = topic_detector.detect(retxt)
-        df_result = topic_detector.get_result(doc)
-
-        return df_result.text.value_counts().index.tolist()[:num_of_topics]
+        df_env_result = topic_detector.get_ent_result(doc)
+        #df_token_result = topic_detector.get_token_result(doc)
+        return df_env_result.text.value_counts().index.tolist()
 
     def generate_xml_text(self):
         text = '{{Infobox Q&A\n'
         text += f'| question_channel = [[チャンネル一覧##{unescape(self.question_channel)}|{unescape(self.question_channel)}]] <!-- チャンネル名 -->\n'
         text += f'| question_date = {self.question_date} <!-- 質問投稿日 -->\n'
-        text += f'| question_member_1 = [[利用者:{unescape(self.question_member)}]] <!-- 質問者 -->\n'
+        text += f'| question_member_1 = [[利用者:{unescape(self.question_user_real_name)}]] <!-- 質問者 -->\n'
 
-        unique_answer_members = set(self.answer_members) \
-            - {self.question_member}
+        unique_answer_real_names = set(self.answer_user_real_names) \
+            - {self.question_user_real_name}
 
-        for i, answer_member in enumerate(unique_answer_members):
-            text += f'| answer_member_{i+1} = [[利用者:{unescape(answer_member)}]]\n'
+        for i, answer_real_name in enumerate(unique_answer_real_names):
+            text += f'| answer_member_{i+1} = [[利用者:{unescape(answer_real_name)}]]\n'
 
         for i in range(min(len(self.tech_topics), 5)):
             text += f'| tech_topic_{i+1} = [[質問トピック::{unescape(self.tech_topics[i])}]]\n'
@@ -109,23 +127,31 @@ class PageDataContainer:
         text += '[[カテゴリ:Q&Aまとめ]]'
         return text
 
-    def to_dict(self, output_template):
-        container_dict = output_template
+    def to_dict(self, base_xml_dict, new_page):
 
-        pg1 = copy.deepcopy(output_template['mediawiki']['page'])
-        pg1['id'] = self.id
-        pg1['title'] = self.title
-        pg1['revision']['text']['#text'] = self.generate_xml_text()
+        new_page['id'] = self.id
+        new_page['title'] = self.title
+        new_page['revision']['text']['#text'] = self.generate_xml_text()
 
-        pg2 = copy.deepcopy(output_template['mediawiki']['page'])
+        base_xml_dict['mediawiki']['page'].append(new_page)
 
-        container_dict['mediawiki']['page'] = [pg1, pg2]
+        return base_xml_dict
 
-        # container_dict['mediawiki']['page']['id'] = self.id
-        # container_dict['mediawiki']['page']['title'] = self.title
-        # container_dict['mediawiki']['page']['revision']['text']['#text'] = self.generate_xml_text()
 
-        return container_dict
+class UserInfoContainer:
+    def __init__(self, df_user_master):
+        self.__display_name_dict = self.set_name_dict(
+            df_user_master, 'display_name')
+        self.__real_name_dict = self.set_name_dict(df_user_master, 'base_name')
+
+    def set_name_dict(self, df_user_info, col_name):
+        return dict(zip(df_user_info['user_id'], df_user_info[col_name]))
+
+    def get_display_name(self, user_id):
+        return self.__display_name_dict.get(user_id)
+
+    def get_real_name(self, user_id):
+        return self.__real_name_dict.get(user_id)
 
 
 class TopicDetector:
@@ -169,12 +195,22 @@ class TopicDetector:
         doc = self.nlp(text)
         return doc
 
-    def get_result(self, doc):
+    def get_ent_result(self, doc):
         if doc is None:
             return []
         df_result = pd.DataFrame([[ent.text, ent.label_, str(ent.start_char), str(ent.end_char)] for ent in doc.ents],
                                  columns=['text', 'label', 'start_pos', 'end_pos'])
         return df_result[df_result['label'].isin(self.target_labels)]
+
+    def get_token_result(self, doc):
+        result_list = []
+        for sent in doc.sents:
+            result_list = result_list + \
+                [[str(token.i), token.text, token.lemma_, token.pos_, token.tag_]
+                 for token in sent]
+        df_result = pd.DataFrame(result_list, columns=[
+                                 'token_no', 'text', 'lemma', ',pos', 'tag'])
+        return df_result
 
 # 投稿本文のusername を置換するための関数
 
@@ -184,18 +220,15 @@ def replace_username(text, user_master) -> 'replaced_text':
     usercodes = re.findall(pattern, text)
 
     for usercode in usercodes:
-        text = text.replace(usercode, f'```{user_master.get(usercode)}```')
+        text = text.replace(
+            usercode, f"''{user_master.get_display_name(usercode)}'''")
     return text
 
 
 def setup(user_master_filepath, output_template_filepath):
 
-    def create_user_master(df) -> {'target_date': {'user_id': 'user_name'}}:
-        user_dict = dict()
-        for td in df['target_date'].unique():
-            df_tmp = df[df['target_date'] == td]
-            user_dict[td] = dict(zip(df_tmp['user_id'], df_tmp['user_name']))
-        return user_dict
+    def create_user_master(df) -> {'target_date': UserInfoContainer}:
+        return {td: UserInfoContainer(df[df['target_date'] == td]) for td in df['target_date'].unique()}
 
     def create_template_xml_dxit(xml) -> 'dict':
         return xmltodict.parse(xml.read())
@@ -204,8 +237,10 @@ def setup(user_master_filepath, output_template_filepath):
 
     with open(output_template_filepath, encoding='utf-8') as xml:
         output_template = create_template_xml_dxit(xml)
+    page_template = copy.deepcopy(output_template['mediawiki']['page'])
+    output_template['mediawiki']['page'] = []
 
-    return (user_master, output_template)
+    return (user_master, output_template, page_template)
 
 
 def df_to_container(i, df, user_master, anotation) -> 'container class':
@@ -226,7 +261,7 @@ def main(input_csv_filepath, user_master_filepath, annotation_master_filepath, o
     max_target_date = df_talks.target_date.max()
 
     # user_master,  xml_template をdict型で読み込み
-    user_master, output_template = setup(
+    user_master, output_template, page_template = setup(
         user_master_filepath, output_template_filepath)
 
     annotation_master = pd.read_csv(annotation_master_filepath)
@@ -237,7 +272,6 @@ def main(input_csv_filepath, user_master_filepath, annotation_master_filepath, o
     topic_detector = TopicDetector(annotation_master, annotation_target_labels)
     container_list = []  # PageDataContainerを格納するリスト
     output_dict_list = []  # 出力dictを格納するdict
-    num_of_pages_in_xml = 1  # 1xmlファイルあたりのページ数 どう使うかよくわからない
 
     # スレッドごとにコンテナクラスのオブジェクトにする
     thread_ts_list = sorted(df_talks['thread_ts'].unique())
@@ -253,12 +287,16 @@ def main(input_csv_filepath, user_master_filepath, annotation_master_filepath, o
             thread_idx, df_thread, user_master, topic_detector)
         container_list.append(container)
         thread_idx += 1
-        break
 
     # 各コンテナをdict形にする
-    for container in container_list:
-        container_dict = copy.deepcopy(container.to_dict(output_template))
-        output_dict_list.append(container_dict)
+    base_dict = copy.deepcopy(output_template)
+    for i, container in enumerate(container_list):
+        base_dict = container.to_dict(base_dict,
+                                      copy.deepcopy(page_template))
+
+        if i % num_of_pages_in_xml == num_of_pages_in_xml - 1:
+            output_dict_list.append(base_dict)
+            base_dict = copy.deepcopy(output_template)
 
     # dictをxmlとして書き出す。
     for i, container_dict in enumerate(output_dict_list):
